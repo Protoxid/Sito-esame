5e34fec76d0905dd4d09fdcbee3404c0df2410bf
Protoxide <raf.mabi@gmail.com>
2015-07-21 10:25:11 +0200

    modifica custom.js

--- js/custom.js
+++ js/custom.js
@@ -42,23 +42,3 @@ $("a.back-to-top").click(function(){
 
     return false;
 })
-
-// lampeggio back to top
-
-jQuery(document).ready(function() {
-    var offset = 220;
-    var duration = 500;
-    jQuery(window).scroll(function() {
-        if (jQuery(this).scrollTop() > offset) {
-            jQuery('.back-to-top').fadeIn(duration);
-        } else {
-            jQuery('.back-to-top').fadeOut(duration);
-        }
-    });
-    
-    jQuery('.back-to-top').click(function(event) {
-        event.preventDefault();
-        jQuery('html, body').animate({scrollTop: 0}, duration);
-        return false;
-    })
-});
